bc75b9c37c482d4848c557e41ee9b8d8ecd0c803
Vyacheslav Kubrak <slvkubrak@gmail.com>
2024-02-14 13:41:34 +0200

    modal

--- app/components/FirstSection.jsx
+++ app/components/FirstSection.jsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import arrow from "@/app/assets/img/Arrow-White.svg";
-import ggg from "../assets/video/gggg.svg";
+import { useState } from "react";
 
 export default function FirstSection() {
+  const [modal, setModal] = useState();
+  const handleOpenModel = () => {
+    setModal(!modal);
+  };
+
   return (
     <section className="container first-section">
-      <h1 className="h1css h1 ">Ukrainian Private Equity & Venture Capital Summit</h1>
+      <h1 className="h1css h1 ">
+        Ukrainian Private Equity & Venture Capital Summit
+      </h1>
       <p className="subtitle">
         March 21, 2024
         <br /> <span>Google Campus Warsaw, Poland</span>{" "}
@@ -14,14 +23,111 @@ export default function FirstSection() {
         The EU-Ukraine Investment Bridge: a meeting place for Ukrainian and
         international investors
       </p>
-      <button className="button">
+      <button onClick={() => handleOpenModel()} className="button">
         I want to attend <Image src={arrow} />
       </button>
       <div className="video-container">
         <video className="video" loop controls preload="auto" autoPlay>
-          <source src="../assets/video/flag.mp4" type="video/mp4" />
+          <source src="../assets/flag.mp4" type="video/mp4" />
         </video>
+        {/* <video className="video" loop controls autoPlay src='./flag.mp4'></video> */}
       </div>
+
+      {modal && (
+        <>
+          <div className="modal">
+            <h2>
+              Registration for the Ukrainian
+              <br /> PE&VC Summit
+            </h2>
+            <form className="form">
+              <div className="input-container">
+                <label className="label" htmlFor="email">
+                  Email <span>*</span>
+                </label>
+                <input
+                  className="input"
+                  type="text"
+                  id="email"
+                  placeholder="Enter your Email"
+                />
+              </div>
+              <div className="input-container">
+                <label className="label" htmlFor="name">
+                  Full name <span>*</span>
+                </label>
+                <input
+                  className="input"
+                  type="text"
+                  id="name"
+                  placeholder="Enter your full name"
+                />
+              </div>
+              <div className="input-container">
+                <label className="label" htmlFor="organization">
+                  Organization <span>*</span>
+                </label>
+                <input
+                  className="input"
+                  type="text"
+                  id="organization"
+                  placeholder="Enter your organization"
+                />
+              </div>
+              <div className="input-container">
+                <label className="label">
+                  Are you agree to participate at one of the panels as a
+                  speaker? <span>*</span>
+                </label>
+                <select className="input" name="speaker" id="">
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+              </div>
+              <div className="input-container">
+                <label className="label">
+                  If you agree, which panel would you like to participate in?{" "}
+                </label>
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="Enter your answer"
+                />
+              </div>
+              <div className="input-container">
+                <label className="label">
+                  Would you like to support the Summit as a sponsor, partner or
+                  in any other way? <span>*</span>
+                  <br />
+                  (We will contact you to discuss the options)
+                </label>
+                <select className="input" name="speaker" id="">
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+              </div>
+              <div className="input-container">
+                <label className="label">
+                  Would you like to promote your company / organization during
+                  the Summit? <span>*</span>
+                  <br />
+                  (We will contact you to discuss the options)
+                </label>
+                <select className="input" name="speaker" id="">
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
+              </div>
+              <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <button className="button">
+                  Send <Image src={arrow} />
+                </button>
+              </div>
+            </form>
+          </div>
+          <div onClick={() => handleOpenModel()} className="overlay"></div>
+        </>
+      )}
     </section>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+
+
       </Head>
       <body>
         <Header />
